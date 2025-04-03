04f2871dcb56dde631723843d36d3107c70672de
HimeshDua <himeshdmaheswary@gmail.com>
2025-04-03 20:58:24 +0500

    add utility functions, theme provider, and navbar components; update Tailwind CSS configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type {Metadata} from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
-import {ClerkProvider} from '@clerk/nextjs';
+import Navbar from '@/components/Navbar';
+import {ClerkProvider} from '@/components/ClerkProviderWrapper';
+import {ThemeWrapper} from '@/components/ThemeProvider';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -25,14 +27,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ClerkProvider>
+          <ThemeWrapper>
+            <div className="min-h-screen">
+              <Navbar />
+              <main className="py-8">
+                <div className="max-w-7xl mx-auto px-4">
+                  <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                    <div className="hidden lg:block lg:col-span-3">Sidebar</div>
+                    <div className="lg:col-span-9">{children}</div>
+                  </div>
+                </div>
+              </main>
+            </div>
+          </ThemeWrapper>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,9 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton
-} from '@clerk/nextjs';
 export default function Home() {
   return (
     <>
-      <header className="flex justify-end items-center p-4 gap-4 h-16">
-        <SignedOut>
-          <SignInButton />
-          <SignUpButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </header>
+      <div className="m-4">
+        <h1>Home page content</h1>
+      </div>
     </>
   );
 }
