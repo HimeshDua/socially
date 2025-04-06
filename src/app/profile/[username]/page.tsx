a6b6a648a28314c7058cd223942f8798a78fe776
HimeshDua <himeshdmaheswary@gmail.com>
2025-04-06 17:08:44 +0500

    just finished this shit

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -30,7 +30,13 @@ export async function generateMetadata({params}: {params: {username: string}}) {
   };
 }
 
-async function ProfilePageServer({params}: {params: {username: string}}) {
+interface PageProps {
+  params: {
+    username: string;
+  };
+}
+
+async function ProfilePageServer({params}: {params: PageProps['params']}) {
   const user = await getProfileByUsername(params.username);
 
   if (!user) notFound();
